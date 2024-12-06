61e90d07dd470fd43c446a5b86ff984e4e7ac2a8
Eda AYDIN <edaaydin.ea@outlook.com>
2024-12-06 23:59:18 +0300

    update square.py

--- Ch02/challenge/square.py
+++ Ch02/challenge/square.py
@@ -15,3 +15,21 @@ img.shape
 # Top-left: 190x350
 # Bottom-right: 680x850
 # Line width: 5
+
+top_left_x, top_left_y = 350, 190
+bottom_right_x, bottom_right_y = 850, 680
+width = 5
+
+color = [0, 0, 0xFF] # blue
+
+# Top line
+img[top_left_x:top_left_x+width, top_left_y:bottom_right_y] = color
+# Bottom line
+img[bottom_right_x:bottom_right_x+width, top_left_y:bottom_right_y] = color
+# Left line
+img[top_left_x:bottom_right_x, top_left_y:top_left_y+width] = color
+# Right line
+img[top_left_x:bottom_right_x, bottom_right_y-width:bottom_right_y] = color
+
+plt.imshow(img)
+# %%
