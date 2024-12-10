9b2637f95fa8e786f7f7a9f66040d4f164ba2168
Eda AYDIN <edaaydin.ea@outlook.com>
2024-12-10 19:46:49 +0300

    update mean_speed.py

--- Ch03/challenge/mean_speed.py
+++ Ch03/challenge/mean_speed.py
@@ -2,21 +2,17 @@
 
 # - Run download_data.py to download the data
 
-# %%
+# - Load the data using pandas
 import pandas as pd
 
-df = pd.read_parquet('taxi.parquet')
+df = pd.read_parquet('./taxi.parquet')
 
-# %%
-mask = df['tpep_dropoff_datetime'] > df['tpep_pickup_datetime']
+# mean speed (mile/hour)
+mask = df["tpep_dropoff_datetime"] > df["tpep_pickup_datetime"]
 df = df[mask]
 
-# %%
-times = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
-times_hour = times / pd.Timedelta(1, 'hour')
-speed = df['trip_distance'] / times_hour
+times = df["tpep_dropoff_datetime"] - df["tpep_pickup_datetime"]
+times_hour = times / pd.Timedelta(1, "hour")
+speed = df["trip_distance"] / times_hour
 
-# %%
-speed.mean()
-
-# %%
+print(speed.mean())
